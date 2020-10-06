9ba92ecf17d4262549f7cadee1fd2484f82843e1
Mikael-R <mikarg9@gmail.com>
2020-10-06 15:32:25 -0300

    refactor: 💡 volume command now get connection and not join

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -1,47 +1,42 @@
-import { PermissionString, VoiceConnection } from 'discord.js'
+import { VoiceState } from 'discord.js'
 
 import { Command, CommandConfig } from '../../types'
 
 class Volume implements Command {
-  private voiceConnection: Promise<VoiceConnection | null>
-  private volume: number | null
+  private memberVoiceState: VoiceState
+  private clientVoiceState?: VoiceState
+  private volume?: number
 
   constructor(private commandConfig: CommandConfig) {
-    const {
-      message: {
-        member: {
-          voice: { channel },
-        },
-      },
-      messageArgs,
-    } = commandConfig
+    const { message, messageArgs } = commandConfig
 
-    this.voiceConnection = channel?.join().catch(() => null)
-    this.volume = Number(messageArgs[1]) || null
+    this.memberVoiceState = message.member.voice
+
+    this.clientVoiceState = this.memberVoiceState.channel?.members.find(
+      ({ id }) => id === message.guild.me.id
+    )?.voice
+
+    this.volume = Number(messageArgs[1])
   }
 
   static commandName = 'volume'
-  static aliases = ['vol', 'vl']
+  static aliases = ['vl', 'vol']
   static description = 'Change or show the current stream volume'
   static minArguments = 0
-  static permissions: PermissionString[] = ['CONNECT', 'SPEAK']
   static usage = 'volume [number, empty]'
   static example = 'volume 7'
 
-  async validator() {
-    const voiceConnection = await this.voiceConnection
-
+  validator() {
     switch (true) {
-      case !voiceConnection:
+      case !this.memberVoiceState.channel:
         return [
           ':red_circle: You need to be on a voice channel to change volume',
         ]
 
-      case this.volume && (this.volume > 10 || this.volume < 0):
-        return [':red_circle: Use value greater than 0 and less than 10']
+      case this.volume && (this.volume > 16 || this.volume < 0):
+        return [':red_circle: Use value greater than 0 and less than 16']
 
-      case !voiceConnection.dispatcher:
-        voiceConnection.disconnect()
+      case !this.clientVoiceState?.connection?.dispatcher:
         return [":red_circle: I'm not stream anything on your voice channel"]
 
       default:
@@ -49,37 +44,28 @@ class Volume implements Command {
     }
   }
 
-  async run() {
+  run() {
     const {
       commandConfig: { embed },
-      voiceConnection,
+      clientVoiceState: {
+        connection: { dispatcher, channel },
+      },
       volume,
     } = this
 
-    await voiceConnection
-      .then(({ dispatcher, channel }) => {
-        const oldVolume = dispatcher.volume * 10
+    const oldVolume = dispatcher.volume * 10
 
-        if (volume) {
-          dispatcher.setVolume(volume / 10)
+    if (volume) {
+      dispatcher.setVolume(volume / 10)
 
-          embed.setDescription(
-            `:nazar_amulet: Changed the volume on the \`\`${channel.name}\`\` channel, **${oldVolume}** to **${volume}**`
-          )
-        } else {
-          embed.setDescription(
-            `:nazar_amulet: The current volume in the \`\`${channel.name}\`\` channel is **${oldVolume}**`
-          )
-        }
-      })
-      .catch(error => {
-        console.error(error)
-        embed.setDescription(
-          ':red_circle: An error occurred while entering the voice channel'
-        )
-        embed.setColor('#E81010')
-      })
-
+      embed.setDescription(
+        `:nazar_amulet: Changed the volume on the \`\`${channel.name}\`\` channel, **${oldVolume}** to **${volume}**`
+      )
+    } else {
+      embed.setDescription(
+        `:nazar_amulet: The current volume in the \`\`${channel.name}\`\` channel is **${oldVolume}**`
+      )
+    }
     return embed
   }
 }
